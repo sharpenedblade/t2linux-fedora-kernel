8ffcdeb918f08f182a952db2d465bf5ff6028b2a
example <example@example.com>
2022-07-13 16:10:35 -0700

    Typo

--- build.sh
+++ build.sh
@@ -32,7 +32,7 @@ tar -xf /root/rpmbuild/SOURCES/linux-*.tar.xz
 cd *
 git init
 git config user.name build
-bit config user.email build@example.com
+git config user.email build@example.com
 git add * .*
 git commit -m "init"
 git clone --depth=1 https://github.com/t2linux/apple-bce-drv drivers/staging/apple-bce
